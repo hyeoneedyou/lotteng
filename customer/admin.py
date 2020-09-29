cf13bbc053323cf7bf49258012e270fa7dfb6696
shinkeonkim <singun11@naver.com>
2020-09-29 23:22:47 +0900

    fix: model issue, admin page setting

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Customer
 
-# Register your models here.
+admin.register(Customer)
--- product/models.py
+++ product/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-class Company(models.Model):
-    name = models.CharField(pk = True, max_length=45)
-    info = models.TextField() # markdown field로 변경예정
-    price = models.IntegerField()
+class Product(models.Model):
+    name = models.CharField(primary_key = True, max_length=45)
+    info = models.TextField(null = True, blank=True) # markdown field로 변경예정
+    price = models.IntegerField(null = True, blank=True)
 
     
