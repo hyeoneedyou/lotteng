0da5c34d5164c1a181562efd500dd181952621b0
shinkeonkim <singun11@naver.com>
2020-10-06 17:26:55 +0900

    fix: admin query content

--- product/admin.py
+++ product/admin.py
@@ -11,12 +11,12 @@ class ProductAdmin(admin.ModelAdmin):
     make_published.short_description = '지정 상품을 배포 상태로 변경'
 
     def make_draft(self, request, queryset):
-        updated_count = queryset.update(status='p') #queryset.update
+        updated_count = queryset.update(status='d') #queryset.update
         self.message_user(request, '{}개의 상품을 준비 중 상태 로 변경'.format(updated_count))
     make_draft.short_description = '지정 상품을 준비 중 상태로 변경'
 
     def make_withdrawn(self, request, queryset):
-        updated_count = queryset.update(status='p') #queryset.update
+        updated_count = queryset.update(status='w') #queryset.update
         self.message_user(request, '{}개의 상품을 철수 상태 로 변경'.format(updated_count))
     make_withdrawn.short_description = '지정 상품을 철수 상태로 변경'
 
