9d7f151c7cf480c6216dd608b5f9ceecbcf4b3bf
xodus1349 <xodus1349@likelion.org>
2020-10-09 17:41:32 +0900

    feat : 결제완료페이지 구현

--- customerPurchase/views.py
+++ customerPurchase/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+import random 
 
 # Create your views here.
+def purchasecode(request):
+    code = random.randrange(1000, 9999)
+    return render(request, 'customerPurchase/purchasecode.html', {'code':code})
