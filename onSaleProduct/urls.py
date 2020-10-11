a3341b7d794d13c6c7e55265d1415f5256a97ecf
shinkeonkim <singun11@naver.com>
2020-10-11 18:16:03 +0900

    feat: detail page urls setting

--- onSaleProduct/urls.py
+++ onSaleProduct/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.show_on_sale_product_list_view, name="show_on_sale_product_list"),
+    path('', views.show_on_sale_product_list, name="show_on_sale_product_list"),
+    path('id:int', views.show_on_sale_product_detail, name="show_on_sale_product_list"),
 ]
