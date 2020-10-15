5b398c7ed7c17d44aafbcf64fae40e9661967a2e
shinkeonkim <singun11@naver.com>
2020-10-16 00:13:42 +0900

    fix: onSaleProduct shop column

--- onSaleProduct/admin.py
+++ onSaleProduct/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import OnSaleProduct
 
 class OnSaleProductAdmin(admin.ModelAdmin):
-    list_display = ['id', 'product', 'kinds', 'price', 'stock', 'endDate', 'createdAt', 'updatedAt']
+    list_display = ['id', 'product', 'shop', 'kinds', 'price', 'stock', 'endDate', 'createdAt', 'updatedAt']
     list_filter = ['product', 'kinds']
 
 admin.site.register(OnSaleProduct, OnSaleProductAdmin)
