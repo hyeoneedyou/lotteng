6bcb264a8eb23de4aea0d8b323d7d5fa28454800
shinkeonkim <singun11@naver.com>
2020-10-17 00:55:56 +0900

    feat: product card detail 페이지 연결

--- onSaleProduct/urls.py
+++ onSaleProduct/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.show_on_sale_product_list, name="show_on_sale_product_list"),
-    path('<int:id>', views.show_on_sale_product_detail, name="show_on_sale_product_list"),
+    path('<int:id>', views.show_on_sale_product_detail, name="show_on_sale_product_detail"),
 ]
