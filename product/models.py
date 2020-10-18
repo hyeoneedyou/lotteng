bb7fac845373c5d90acf525af6930dad1466da58
shinkeonkim <singun11@naver.com>
2020-10-18 23:00:42 +0900

    feat: product 대표 이미지 represent_image field 추가

--- product/models.py
+++ product/models.py
@@ -10,6 +10,7 @@ class Product(models.Model):
 
     name = models.CharField(primary_key = True, max_length=45)
     info = MDTextField(null = True, blank=True)
+    represent_image = models.ImageField(blank = True, upload_to = "product/")
     sub_name = models.TextField(null = True, blank=True)
     price = models.IntegerField(null = True, blank=True)
     status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='d')
