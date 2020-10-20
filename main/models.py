83380aeaf474a89d227db37ee5946741390320b1
JeongKwangSu <soo0546@naver.com>
2020-10-20 17:18:38 +0900

    feat : map

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# 지도에 표시될 각 매장의 정보 모델
+class ShopInfo(models.Model):
+    objects = models.Manager()
+    name = models.CharField(max_length=45)          #name = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name="shopName")
+    latitude = models.CharField(max_length=45)      #latitude = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name="lat")
+    longitude = models.CharField(max_length=45)     #longitude = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name="lng")
+    company = models.CharField(max_length=45)       #company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="company")
--- main/views.py
+++ main/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from onSaleProduct.views import get_on_sale_product_list
+from main.models import ShopInfo
 # Create your views here.
 def home(request):
     res = {}
-    res.update(get_on_sale_product_list(1))
+    res.update(get_on_sale_product_list(1)) 
+    shopinfolist = ShopInfo.objects.all() #지도에 표시될 각 매장의 정보 list
+    context = {'shopInfoList' : shopinfolist}
+    res.update(context) #res에 추가
     return render(request, 'home.html', res)
 
 def show_team_profile(request):
