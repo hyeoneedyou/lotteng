0ef3a35b9ff50f9ad99c33fc71e3a15b3f3b287c
shinkeonkim <singun11@naver.com>
2020-10-22 04:12:03 +0900

    feat: shopping car 목록상의 버튼 ajax 연동

--- shoppingCart/urls.py
+++ shoppingCart/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import put_shopping_cart, shoppingcart_list, put_one_product
 
 urlpatterns = [
-    path('<int:onSaleProduct_id>', put_one_product, name="put_one_product"),
+    path('put_one_product', put_one_product, name="put_one_product"),
     path('put_shopping_cart', put_shopping_cart, name="put_shopping_cart"),
     path('', shoppingcart_list, name="shoppingcart_list"),
 ]
